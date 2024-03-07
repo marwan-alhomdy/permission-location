b0aac3f9a47613d37a277ab649669226215ff858
marwan alhomdy <137527596+marwan-alhomdy@users.noreply.github.com>
2024-03-07 08:37:15 +0300

    Update check-location.dart

--- check-location.dart
+++ check-location.dart
@@ -9,3 +9,43 @@
         shape: BorderRadiusAttributes.onlyTopRounded(30),
       );
     }
+
+
+
+class LocationSwitcherWidget extends StatelessWidget {
+  const LocationSwitcherWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const LineButtonSheetWidget(),
+        Text(
+          'تحتاج إلى تشغيل الموقع',
+          style: AppTextStyles.getMediumStyle(),
+        ),
+        const SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            'يحتاج التطبيق إلى تشغيل خدمة الموقع لكي يعمل بشكل صحيح. هل تريد تشغيلها الآن؟',
+            style: AppTextStyles.getRegularStyle(),
+          ),
+        ),
+        const SizedBox(height: 10),
+        ButtonPermissonWidget(
+          name: "تشغيل الموقع".tr,
+          icon: IconlyBold.location,
+          onTap: () async {
+            await Geolocator.openLocationSettings();
+            // ignore: use_build_context_synchronously
+            Navigator.of(context).pop();
+          },
+        ),
+        const SizedBox(height: 30),
+      ],
+    );
+  }
+}
